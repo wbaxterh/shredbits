72f717f34a5bdf433eb996db28f5b02de664ccbf
Wes Huber <huberwb@g.cofc.edi>
2018-08-06 14:58:10 -0400

    html

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
