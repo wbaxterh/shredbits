190c99922c4de1959cadd28092439263cece4540
Wes Huber <huberwb@g.cofc.edi>
2018-10-16 00:49:11 -0400

    setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,30 @@
 module.exports = {
-  apps: [{
-    name: 'shredbits',
-    script: './index.js'
+  apps : [{
+    name: 'API',
+    script: './index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }],
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: 'ec2-18-218-211-200.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/bitcoin-tracker.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:wbaxterh/shredbits.git',
-      path: '/home/ubuntu/shredbits-2',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-18-218-211-200.us-east-2.compute.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:wbaxterh/shredbits.git',
+      path : '/home/ubuntu/tutorial-2',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
-}
+};
