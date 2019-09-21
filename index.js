617d4df91c8fb7a1441059011579257f72f706d4
Wes Huber <huberwb@g.cofc.edi>
2019-09-21 00:02:00 -0400

    new commitee

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const express = require('express')
-//const app = express()
+const express = require('express')
+const app = express()
 //app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
