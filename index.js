bdd9a3821ad5f0fa78a9c40d46034861227f542d
Wes Huber <huberwb@g.cofc.edi>
2019-09-20 23:52:40 -0400

    fix attempt

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-//const express = require('express')
-//const app = express()
-//app.use(express.static('public'))
-var request = require("request");
-
-var options = { method: 'GET',
-  url: 'https://globalmagnet.amentum.space/api/calculate_magnetic_field',
-  qs: 
-   { altitude: '%200.0618744',
-     longitude: '-83.278831',
-     latitude: '30.830750',
-     year: '2019' },
-  headers: 
-   { 'Postman-Token': 'fab01bea-063b-4e18-875c-1615be6a5d33',
-     'cache-control': 'no-cache' } };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
-
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
